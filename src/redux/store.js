ee702820c924332e0816feaaf99aaac5661e6038
vladimir_pisacic <vladimirpisacic@gmail.com>
2020-04-21 14:24:21 -0700

    moved our direcotry and shop data into its own reducer ,createdrespective selectors and updeted corresponding components with new redax flow for directory-shop

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware} from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
@@ -7,6 +8,6 @@ import rootReducer from './root-reducer';
 const middlewares = [logger];
 
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
+export default {store,persistor};
